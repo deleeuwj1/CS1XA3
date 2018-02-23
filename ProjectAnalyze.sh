b8def7b7d375ade3a8ba406339e9c7884b5e9d23
Jessica de Leeuw <deleeuwj@mills.cas.mcmaster.ca>
2018-02-23 11:24:14 -0500

    Changed each section into a separate function and fixed findTODO

--- ProjectAnalyze.sh
+++ ProjectAnalyze.sh
@@ -1,46 +1,50 @@
 #!/bin/bash
 
 #informs you if the local repo is up to date with the remote repo
-$(git fetch origin)
-localRepo=(git rev-parse master)
-remoteRepo=(git ls-remote)
-if [ $localRepo == $remoteRepo ]
+function isUpToDate () {
+    $(git fetch origin)
+    localRepo=(git rev-parse master)
+    remoteRepo=(git rev-parse origin/master)
+    if [ $localRepo == $remoteRepo ]
+    then
+        echo "Local Repo is up to date with Remote Repo"
+    else
+        echo "Local Repo is not up to date with Remote Repo"
+    fi
+}
 
-then
-    echo "Local Repo is up to date with Remote Repo"
-else
-    echo "Local Repo is not up to date with Remote Repo"
-fi
+function uncommittedChanges () {
+    git diff > changes.log
+}
 
-#puts all uncommitted changes in changes.log
-git diff $1 > changes.log
-
-#puts each line from every file with the tag #TODO into todo.log
-todo=$( grep -r "#TODO" --exclude=todo.log )
-$todo > todo.log 
+function findTODO () {
+    grep -r -e "#TODO" --exclude="todo.log" --exclude="ProjectAnalyze.sh" > todo.log 
+}
 
 #checks all haskell files for syntax errors and puts the results into error.log
-f=$1
-for f in "*.hs"
-do
-    ghc -fno-code $f 2> error.log
-done
+function haskellErrors () {
+    f=$1
+    for f in "*.hs"
+    do
+        ghc -fno-code $f 2> error.log
+    done
+}
 
-#moves up n number of dirtories
-function movingUpDirectories () {
-    echo "Would you like to move up directories? (Y/N)"
-    read ans
-    if [ $ans = "Y" ]
+#moves up n number of directories
+function moveUp () {
+    read -p "Would you like to move up directories? (Y/N)" ans
+    if [ $ans == "Y" ]
     then 
-        echo "How many directories would you like to move up?"
-	read num
+        read -p "How many directories would you like to move up?" num
         i=0
         d=$PWD
-        while [ i -lt num ]
+        while [ i < $num ]
         do
-	    d=$d/..
+	    d=$d../
             i=i+1
         done
         cd d
     fi
 }
+
+"$@"
