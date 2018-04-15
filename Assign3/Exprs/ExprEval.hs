767b225c8e571601154653a0a9af011245446c87
Jessica de Leeuw <deleeuwj@mills.cas.mcmaster.ca>
2018-04-15 11:08:36 -0400

    Updated simplify

--- Assign3/Exprs/ExprEval.hs
+++ Assign3/Exprs/ExprEval.hs
@@ -99,86 +99,71 @@ module ExprEval where
 
    {- simplify expressions -}
    --addition
-   simplify vrs (Add e (Var x)) = case Map.lookup x vrs of
-                                    Just v  -> (Add (simplify vrs e) (Const v))
-                                    Nothing -> (Add (simplify vrs e) (Var x))
-   simplify vrs (Add (Var x) e) = simplify vrs (Add e (Var x))
-   simplify vrs (Add e1 e2)     = Add (simplify vrs e1) (simplify vrs e2)
+   simplify vrs (Add e (Var x))           = Add (simplify vrs e) (Var x)
+   simplify vrs (Add (Var x) e)           = simplify vrs (Add e (Var x))
+   simplify vrs (Add (Const 0) e)         = simplify vrs e
+   simplify vrs (Add e (Const 0))         = simplify vrs e
+   simplify vrs (Add (Const a) (Const b)) = Const (a + b)
+   simplify vrs (Add e1 e2)               = Add (simplify vrs e1) (simplify vrs e2)
 
    --subtraction
-   simplify vrs (Sub e (Var x)) = case Map.lookup x vrs of
-                                    Just v  -> (Sub (simplify vrs e) (Const v))
-                                    Nothing -> (Sub (simplify vrs e) (Var x))
-   simplify vrs (Sub (Var x) e) = case Map.lookup x vrs of
-                                    Just v  -> (Sub (Const v) (simplify vrs e))
-                                    Nothing -> (Sub (Var x) (simplify vrs e))
-   simplify vrs (Sub e1 e2)     = Sub (simplify vrs e1) (simplify vrs e2)
+   simplify vrs (Sub e (Var x))           = Sub (simplify vrs e) (Var x)
+   simplify vrs (Sub (Var x) e)           = Sub (Var x) (simplify vrs e)
+   simplify vrs (Sub e (Const 0))         = simplify vrs e
+   simplify vrs (Sub (Const a) (Const b)) = Const (a - b)
+   simplify vrs (Sub e1 e2)               = Sub (simplify vrs e1) (simplify vrs e2)
 
    -- multiplication
-   simplify vrs (Mult e (Var x)) = case Map.lookup x vrs of
-                                    Just v  -> (Mult (simplify vrs e) (Const v))
-                                    Nothing -> (Mult (simplify vrs e) (Var x))
-   simplify vrs (Mult (Var x) e) = simplify vrs (Mult e (Var x))
-   simplify vrs (Mult e1 e2)     = Mult (simplify vrs e1) (simplify vrs e2)
+   simplify vrs (Mult e (Var x))           = Mult (simplify vrs e) (Var x)
+   simplify vrs (Mult (Var x) e)           = simplify vrs (Mult e (Var x))
+   simplify vrs (Mult (Const 0) e)         = Const 0
+   simplify vrs (Mult e (Const 0))         = Const 0
+   simplify vrs (Mult (Const 1) e)         = simplify vrs e
+   simplify vrs (Mult e (Const 1))         = simplify vrs e
+   simplify vrs (Mult (Const a) (Const b)) = Const (a * b)
+   simplify vrs (Mult e1 e2)               = Mult (simplify vrs e1) (simplify vrs e2)
 
    -- division
-   simplify vrs (Div e (Var x))       = case Map.lookup x vrs of
-                                          Just v  -> (Div (simplify vrs e) (Const v))
-                                          Nothing -> (Div (simplify vrs e) (Var x))
-   simplify vrs (Div (Var x) e)       = case Map.lookup x vrs of
-                                          Just v  -> (Div (Const v) (simplify vrs e))
-                                          Nothing -> (Div (Var x) (simplify vrs e))
-   {-
-   simplify vrs (Div (Var a) (Var b)) = case (Map.lookup a vrs, Map.lookup b vrs) of
-                                          (Just x, Just y)   -> (Div (Const x) (Const y))
-                                          (Just x, _)        -> (Div (Const x) (Var b))
-                                          (_, Just y)        -> (Div (Var a) (Const y))
-                                          (Nothing, Nothing) -> if a == b
-                                                                then (Const 1)
-                                                                else (Div (Var a) (Var b)) -}
-   simplify vrs (Div e1 e2)           = Div (simplify vrs e1) (simplify vrs e2)
+   simplify vrs (Div (Var x) (Var y))    = if x == y
+                                           then (Const 1)
+                                           else (Div (Var x) (Var y))
+   simplify vrs (Div e (Var x))           = Div (simplify vrs e) (Var x)
+   simplify vrs (Div (Var x) e)           = Div (Var x) (simplify vrs e)
+   simplify vrs (Div e (Const 1))         = simplify vrs e
+   simplify vrs (Div (Const 0) _)         = Const 0
+   simplify vrs (Div (Const a) (Const b)) = Const (a / b)
+   simplify vrs (Div e1 e2)               = Div (simplify vrs e1) (simplify vrs e2)
 
    -- natural exponent
-   simplify vrs (E (Var x)) = case Map.lookup x vrs of
-                                Just v  -> (E (Const v))
-                                Nothing -> (E (Var x))
+   simplify vrs (E (Var x)) = E (Var x)
    simplify vrs (E (Ln e))  = simplify vrs e
    simplify vrs (E e)       = E (simplify vrs e)
 
-
    -- logarithm of any base
-   simplify vrs (Log a (Var x)) = case Map.lookup x vrs of
-                                    Just v  -> (Log a (Const v))
-                                    Nothing -> (Log a (Var x))
-   simplify vrs (Log a e)       = Log a (simplify vrs e)
+   simplify vrs (Log a (Var x))   = Log a (Var x)
+   simplify vrs (Log a (Const x)) = Const (logBase a x)
+   simplify vrs (Log a e)         = Log a (simplify vrs e)
 
    -- natural logarithm
-   simplify vrs (Ln (Var x)) = case Map.lookup x vrs of
-                                Just v  -> (Ln (Const v))
-                                Nothing -> (Ln (Var x))
-   simplify vrs (Ln (E e))   = simplify vrs e
-   simplify vrs (Ln e)       = Ln (simplify vrs e)
+   simplify vrs (Ln (Var x))   = Ln (Var x)
+   simplify vrs (Ln (E e))     = simplify vrs e
+   simplify vrs (Ln (Const a)) = Const (log a)
+   simplify vrs (Ln e)         = Ln (simplify vrs e)
 
    -- cosine
-   simplify vrs (Cos (Var x)) = case Map.lookup x vrs of
-                                  Just v  -> (Cos (Const v))
-                                  Nothing -> (Cos (Var x))
+   simplify vrs (Cos (Var x)) = Cos (Var x)
    simplify vrs (Cos e)       = Cos (simplify vrs e)
 
    --sine
-   simplify vrs (Sin (Var x)) = case Map.lookup x vrs of
-                                  Just v  -> (Sin (Const v))
-                                  Nothing -> (Sin (Var x))
+   simplify vrs (Sin (Var x)) = Sin (Var x)
    simplify vrs (Sin e)       = Sin (simplify vrs e)
 
    -- exponent
-   simplify vrs (Pow (Var x) e) = case Map.lookup x vrs of
-                                    Just v  -> (Pow (Const v) (simplify vrs e))
-                                    Nothing -> (Pow (Var x) (simplify vrs e))
-   simplify vrs (Pow e (Var x)) = case Map.lookup x vrs of
-                                    Just v  -> (Pow (simplify vrs e) (Const v))
-                                    Nothing -> (Pow (simplify vrs e) (Var x))
-   simplify vrs (Pow e1 e2)     = Pow (simplify vrs e1) (simplify vrs e2)
+   simplify vrs (Pow (Var x) e)   = Pow (Var x) (simplify vrs e)
+   simplify vrs (Pow e (Var x))   = Pow (simplify vrs e) (Var x)
+   simplify vrs (Pow e (Const 0)) = Const 1
+   simplify vrs (Pow e (Const 1)) = simplify vrs e
+   simplify vrs (Pow e1 e2)       = Pow (simplify vrs e1) (simplify vrs e2)
 
    -- constants and variables
    simplify vrs (Const x)    = Const x
